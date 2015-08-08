13aab1651ffda1c648caaa5f71ff71ec62f37a42
C457 <android.c357@gmail.com>
2015-08-08 11:23:23 -0500

    fix build and enable use of device specific cam params

--- patch.sh
+++ patch.sh
@@ -5,11 +5,8 @@
 # BIONIC
 cd bionic
 
-# Partial revert "Remove ioprio_get(2) and ioprio_set(2) from LP64."
-git fetch https://github.com/CyanogenMod/android_bionic cm-12.1
-git cherry-pick 193c39ce03b9897397fe214f948c1fdbd6fa9899
-
 # bionic: Add flag to restore legacy mmap behavior
+git fetch https://github.com/CyanogenMod/android_bionic cm-12.1
 git cherry-pick aa729cb8dd274ea5a6bfc6055a035780478b2d68
 cd -
 
@@ -22,6 +19,11 @@ git cherry-pick d4848efbd5650a48f93a784e5de248a13b830c04
 
 # stagefright: Add legacy MediaBuffer::acquireBuffer symbol
 git cherry-pick 7af55c5b5cf6a1369bc72869913703536c4faae9
+
+# camera: allow device to append camera parameters
+# camera: Fix inclusion of CameraParametersExtra.h
+git cherry-pick 8b850c19f85c0c0512acee4b2899f7daf4c7aca5
+git cherry-pick 9246d55636b5072277c1ea04376426f6ae956edd
 cd -
 
 cd frameworks/native
