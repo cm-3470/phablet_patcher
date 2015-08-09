0ade05ccb3079656f32f58cf87a6c9f94fd0e2d2
C457 <android.c357@gmail.com>
2015-08-08 22:40:42 -0500

    update

--- patch.sh
+++ patch.sh
@@ -2,42 +2,24 @@
 
 # Grab patches for Ubuntu
 
-# BIONIC
-cd bionic
-
-# bionic: Add flag to restore legacy mmap behavior
-git fetch https://github.com/CyanogenMod/android_bionic cm-12.1
-git cherry-pick aa729cb8dd274ea5a6bfc6055a035780478b2d68
-cd -
-
 # BUILD
 cd build
 
 # Allow a device to generically define its own headers
-git fetch https://github.com/CyanogenMod/android_build.git cm-12.1
-git cherry-pick fbd04b338a856b8c484946d748202d6aa6b27158
+git fetch https://github.com/cm-3470/android_build phablet
+git cherry-pick 393177ad9e5e03437b2fd0981a14b1841ce3d406
 cd -
 
 # FRAMEWORK
 cd frameworks/av
 
-# Add Samsung WFD Service -- required for HWComposer
-git fetch https://github.com/cm-3470/android_frameworks_av.git phablet
-git cherry-pick eebc8e8db34b41e1dff2a9a5195642b3650aeb93
-
-git fetch https://github.com/CyanogenMod/android_frameworks_av.git cm-12.0
-# stagefright: Add legacy MediaBuffer::acquireBuffer symbol
-git cherry-pick 7af55c5b5cf6a1369bc72869913703536c4faae9
-
 # camera: allow device to append camera parameters
+git fetch https://github.com/cm-3470/android_frameworks_av.git phablet
+git cherry-pick 0761ee7bd3ccca8b0407993c9e97a85f3de598b5
+
 # camera: Fix inclusion of CameraParametersExtra.h
-git cherry-pick 8b850c19f85c0c0512acee4b2899f7daf4c7aca5
-git cherry-pick 9246d55636b5072277c1ea04376426f6ae956edd
-cd -
+git cherry-pick f4a3f3432f6506b41d6ab61a12884cf1b7786f46
 
-cd frameworks/native
-
-# Revert "Remove Parcel::writeIntPtr."
-git fetch https://github.com/cm-3470/android_frameworks_native.git cm-12.1
-git cherry-pick 4a7379958e9da92b69e359e541f809efd64ba006
+# Add Samsung WFD Service -- required for HWComposer
+git cherry-pick f24e326355824a60cd4b926e8d48ba15cc17ab06
 cd -
